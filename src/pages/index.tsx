21909d86ec9e33cadf128bb3107c578f8d9af788
lance-malaga <lmalaga@my.bcit.ca>
2024-02-17 09:39:31 -0800

    add components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import styles from '@/styles/Home.module.css'
+
+// components
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import CustomHead from '@/components/CustomHead';
+import PageTitle from '@/components/PageTitle';
+import ChooseCity from '@/components/ChooseCity';
 
 export default function Home() {
 	return (
@@ -11,13 +15,8 @@ export default function Home() {
 				<div className={styles.content__container}>
 					<Header/>
 					<main>
-						<div className={styles.title__container}>
-							<h1>City Of Vancouver</h1>
-							<p>Welcome to Venture! Explore Vancouver's diverse neighborhoods to find the perfect match for your lifestyle.</p>
-						</div>
-						<div>
-
-						</div>
+						<PageTitle desc={`Welcome to Venture! Explore Vancouver's diverse neighborhoods to find the perfect match for your lifestyle.`}/>
+						<ChooseCity/>
 					</main>
 				</div>
 			</div>
