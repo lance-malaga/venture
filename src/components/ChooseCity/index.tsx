73bc0fce4c886982347afbfe8f1d2dd3233e2807
lance-malaga <lmalaga@my.bcit.ca>
2024-02-17 12:19:26 -0800

    add city pictures

--- src/components/ChooseCity/index.tsx
+++ src/components/ChooseCity/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './ChooseCity.module.css'
 
 import cityMap from '../../../public/images/map-frame.png'
+import { cities } from '@/data/cities'
 
 export default function ChooseCity() {
     return (
@@ -9,6 +10,17 @@ export default function ChooseCity() {
             <div className={styles.side_nav}>
                 <h5>CHOOSE A CITY</h5>
                 <hr/>
+                {cities.map((data, index) => (
+                    <div key={index} className={styles.city}>
+                        <p>{data.city}</p>
+                        <Image 
+                            src={data.image}
+                            alt={data.city}
+                            width={150}
+                            height={180}
+                        />
+                    </div>
+                ))}
             </div>
             <Image
                 src={cityMap}
