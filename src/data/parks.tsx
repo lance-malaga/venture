90d0ae31958bc494241c8d995d3b74361cdc3336
aneetromana <aneetromana167@gmail.com>
2024-02-20 10:36:21 -0800

    new files

---
+++ src/data/parks.tsx
@@ -0,0 +1,80 @@
+export const parks = [
+    {
+      city: 'Burnaby',
+      parks: [
+        'Deer Lake Park',
+        'Central Park',
+        'Burnaby Mountain',
+        'Burnaby Lake',
+        'Confederation Park',
+        'Byrne Creek Ravine Park',
+      ],
+    },
+    {
+      city: 'Richmond',
+      parks: [
+        'Garry Point Park',
+        'Minoru Park',
+        'Iona Beach Regional Park',
+        'Richmond Nature Park',
+        'Terra Nova Rural Park',
+        'Middle Arm Waterfront Greenway',
+        'Aberdeen Neighbourhood Park',
+      ],
+    },
+    {
+      city: 'Westvan',
+      parks: [
+        'Ambleside Park',
+        'Caulfeild Park',
+        'Cypress Falls Park',
+        'Dundarave Park',
+        'Lighthouse Park',
+        'Horshoe Bay Park',
+      ],
+    },
+    {
+      city: 'New West',
+      parks: [
+        'Queens Park',
+        'Moody Park',
+        'Hume Park',
+        'Quayside Park',
+        'Tipperary Park',
+      ],
+    },
+    {
+      city: 'Delta',
+      parks: [
+        'Diefenbaker Park',
+        'Annieville Lions Park',
+        'Memorial Park',
+        'Watershed Park',
+        'Deas Island Regional Park',
+      ],
+    },
+    {
+      city: 'Coquitlam',
+      parks: [
+        'Town Centre Park',
+        'Powerhouse Park',
+        'Crystal Falls',
+        'Coquitlam Crunch',
+        'Cates Park',
+      ],
+    },
+    {
+      city: 'Surrey',
+      parks: [
+        'Holland Park',
+        'Fleetwood Park',
+        'Surrey Bend Regional Park',
+        'Bear Creek Park',
+        'Tynehead Park',
+        'Blackie Spit Park',
+        'Unwin Park',
+        'Northview Park',
+        'Surrey Bend Park'
+      ],
+    },
+  ];
